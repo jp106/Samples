78598bf2b027a88877bac0a8afc134ea79ebcee2
jp106 <jhansitheplanz@gmail.com>
2017-04-28 08:33:34 -0400

    Code cleanup
    Code cleanup

--- ArcGISJavaScriptAPI/NationalWeatherServiceApplication/NationalWeatherServiceApplication/js/main.js
+++ ArcGISJavaScriptAPI/NationalWeatherServiceApplication/NationalWeatherServiceApplication/js/main.js
@@ -10,13 +10,12 @@ angular.module('weather-service-map', ['esri.map'])
             "esri/symbols/SimpleMarkerSymbol",
             "esri/layers/Layer",
             "esri/Graphic",
-             "esri/widgets/Expand",
-            "dojo/_base/array",
+            "esri/widgets/Expand",
             "dojo/on",
             "dojo/dom",
             "dojo/domReady!"
         ], function (Map, FeatureLayer, ScaleBar, Search,
-            LayerList, SimpleMarkerSymbol, Layer, Graphic, Expand, arrayUtils, on, dom) {
+            LayerList, SimpleMarkerSymbol, Layer, Graphic, Expand, on, dom) {
             //setup the map viewer
             self.map = new Map({
                 basemap: 'streets'
@@ -26,6 +25,7 @@ angular.module('weather-service-map', ['esri.map'])
                 url: "http://services1.arcgis.com/0MSEUqKaxRlEPj5g/ArcGIS/rest/services/Redlands_CA/FeatureServer/0"
             });
             //setup widgets
+            var weatherapiurl = "https://api.weather.gov/points/"
             var scale = new ScaleBar();
             var search = new Search();
             var infoContainer, expandWidget;
@@ -78,20 +78,35 @@ angular.module('weather-service-map', ['esri.map'])
                             }
                             var featurefound = response.results[0].graphic;
                             $scope.statusmessage = "Feature is a " + featurefound.attributes.LocationType + " with ID " + featurefound.attributes.FID;
-                            var newgraphic = featurefound.clone();
-                            newgraphic.symbol = selectionSymbol;
-                            self.mapview.graphics.add(newgraphic);
-
-                            //get feature geometry to get weather data
-                            var latitude = response.results[0].mapPoint.latitude;
-                            var longitude = response.results[0].mapPoint.longitude;
-                            var clickpoint = latitude + "," + longitude;
-                            console.log("Map point clicked: ");
-                            console.log(clickpoint);
-                            var requesturl = "https://api.weather.gov/points/" + clickpoint;
-                            var requestforecast = "https://api.weather.gov/points/" + clickpoint + "/forecast";
+                            addFeaturetomap(featurefound.clone());
+                            //construct request url
+                            var requesturl = setRequestUrl(response.results[0].mapPoint);
                             //get data from weather service
-                            weatherFactory.getPointWeatherDetails(requesturl)
+                            requestWeatherData(requesturl);
+                        });
+                }
+
+                function setRequestUrl(point) {
+                    //get feature geometry to get weather data
+                    var latitude = point.latitude;
+                    var longitude = point.longitude;
+                    var clickpoint = latitude + "," + longitude;
+                    console.log("Map point clicked: ");
+                    console.log(clickpoint);
+                    var requesturl = weatherapiurl + clickpoint;
+                    return requesturl
+                }
+
+                //Add selected feature to map
+                function addFeaturetomap(featuregraphic) {
+                    featuregraphic.symbol = selectionSymbol;
+                    self.mapview.graphics.add(featuregraphic);
+                }
+
+                //weather
+                function requestWeatherData(requesturl) {
+                    var requestforecast = requesturl + "/forecast";
+                    weatherFactory.getPointWeatherDetails(requesturl)
                                 .then(function (response) {
                                     $scope.radarstation = response.radarStation;
                                     weatherFactory.getforecastOffice(response.forecastOffice)
@@ -106,12 +121,12 @@ angular.module('weather-service-map', ['esri.map'])
                                 }, function (reason) {
                                     $scope.error = "Could not find weather data.";
                                 });
-                        });
                 }
+
                 //reset controls and graphics on map
                 var clearmessages = function () {
                     self.mapview.graphics.removeAll();
-                    $scope.statusmessage = "No features found.";
+                    $scope.statusmessage = "Features not found.";
                     $scope.radarstation = " ";
                     $scope.forecastoffice = " ";
                     $scope.forecast = " ";
